38d5b330095f55c9ec60905a42a6f37f30356ef0
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-08 21:31:03 +0800

    12-1 建立貪食蛇Android專案與類別庫導入,畫面設計-1 (貪食蛇遊戲 MVVM)

--- app/src/main/java/com/example/snakegreenbeansoup20220408/SnakeMainActivity.kt
+++ app/src/main/java/com/example/snakegreenbeansoup20220408/SnakeMainActivity.kt
@@ -12,7 +12,7 @@ import com.example.snakegreenbeansoup20220408.databinding.ActivitySnakeMainBindi
 /**
  *  學對之路:Kotlin Android APP 開發
  *  12-1 建立貪食蛇Android專案與類別庫導入,畫面設計 (貪食蛇遊戲 MVVM)
- *      1. 添加Libs (build.gradle)
+ *      1.  添加Libs (build.gradle)
  *      plugins {
  *          id 'kotlin-kapt' // Android Jetpack
  *          id 'kotlin-android-extensions'
@@ -27,8 +27,11 @@ import com.example.snakegreenbeansoup20220408.databinding.ActivitySnakeMainBindi
  *          // Annotation processor (標示語法的類別庫)
  *          kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
  *      }
- *
- *
+ *      2.  下載/抓取貪食蛇上下左右圖示(參考網址:https://www.iconfinder.com/search?q=arrow&price=free),將圖示分別命名(down,left,right,top) , 並將圖示放在drawable資料夾下
+ *      3.  content_snake_main.xml:
+ *      3-1.添加畫面分割線 : Guidelines(上方按鈕) > 選擇Horizontal Guidelines > 分割線點擊兩下變成百分比 > 拉到70%左右
+ *      3-2.拉一個TextView , ID : score ,命名 : 分數(Score)
+ *      3-3.拉ImageView , 放在分割線下方 , 並修改對應 ImageView ID
  *
  */
 
