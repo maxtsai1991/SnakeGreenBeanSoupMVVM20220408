60803adbfa1da601a23a5c280fb39ac95c376464
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-08 20:46:55 +0800

    12-1 建立貪食蛇Android專案與類別庫導入,畫面設計 (貪食蛇遊戲 MVVM)

--- app/src/main/java/com/example/snakegreenbeansoup20220408/SnakeMainActivity.kt
+++ app/src/main/java/com/example/snakegreenbeansoup20220408/SnakeMainActivity.kt
@@ -9,6 +9,29 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.snakegreenbeansoup20220408.databinding.ActivitySnakeMainBinding
 
+/**
+ *  學對之路:Kotlin Android APP 開發
+ *  12-1 建立貪食蛇Android專案與類別庫導入,畫面設計 (貪食蛇遊戲 MVVM)
+ *      1. 添加Libs (build.gradle)
+ *      plugins {
+ *          id 'kotlin-kapt' // Android Jetpack
+ *          id 'kotlin-android-extensions'
+ *      }
+ *
+ *      dependencies {
+ *          def lifecycle_version = "2.5.0-alpha04"
+ *          // ViewModel
+ *          implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+ *          // LiveData
+ *          implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+ *          // Annotation processor (標示語法的類別庫)
+ *          kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+ *      }
+ *
+ *
+ *
+ */
+
 class SnakeMainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
